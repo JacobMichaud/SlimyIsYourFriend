48e8e1283078514eaab23b807107c1168ca1fec7
pokeruler <kevinmujica26@gmail.com>
2024-01-13 21:41:30 -0500

    Added vertical movement and removed gravity and jump

---
+++ character.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	#Vertical movement (needs the speed to be changed a bit so it feels more realistic)
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = -SPEED * 0.9
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = SPEED * 0.9
+	else: 
+		velocity.y = 0
+	
+	move_and_slide()
