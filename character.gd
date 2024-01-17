80078c61a978f4985329c5621ed31bb72cb40b1c
JacobMichaud <110305423+JacobMichaud@users.noreply.github.com>
2024-01-16 19:56:05 -0500

    added walking animation and wasd movement

--- character.gd
+++ character.gd
@@ -7,19 +7,27 @@ const JUMP_VELOCITY = -400.0
 func _process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if velocity.x >0:
+		anim.flip_h = false
+		anim.play("Walk")
+	elif velocity.x <0:
+		anim.flip_h = true
+		anim.play("Walk")
 	
 	#Vertical movement (needs the speed to be changed a bit so it feels more realistic)
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("up"):
 		velocity.y = -SPEED * 0.9
-	elif Input.is_action_pressed("ui_down"):
+		anim.play("Walk")
+	elif Input.is_action_pressed("down"):
 		velocity.y = SPEED * 0.9
-	else: 
-		anim.play("default")
+		anim.play("Walk")
+	else: 	
 		velocity.y = 0
-	
+	if velocity.x ==0 && velocity.y ==0:
+		anim.play("idle")	
 	move_and_slide()
